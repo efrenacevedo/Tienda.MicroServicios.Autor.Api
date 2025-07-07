b78906fa8d9e45324ba05017421084b6475625b7
Efren <joseefrenacevedo3@gmail.com>
2025-07-07 16:17:09 -0600

    otros cambios

--- Program.cs
+++ Program.cs
@@ -3,7 +3,20 @@ using Tienda.MicroServicios.Autor.Api.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 
 // Definir orígenes permitidos (pon aquí el dominio real de tu frontend)
-var allowedOrigins = new[] { "https://vistalibrosautores-production.up.railway.app" };
+var allowedOrigins = new[] {
+    "https://vista-libros-autores-xge2-efrenacevedos-projects.vercel.app"
+};
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins(allowedOrigins)
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 
 builder.Services.AddControllers();
 
